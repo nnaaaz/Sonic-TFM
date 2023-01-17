b3708b6b99431d50bd98c9a227bf23f2fe6bcc4b
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-17 23:41:20 +0100

    underscore

--- lua/bonus_score10.lua
+++ lua/bonus_score10.lua
@@ -9,9 +9,9 @@ local image_list = pshy.require("pshy.images.list")
 image_list["185bbbc0145.png"] = {bonus = true, w = 30, h = 30, desc = "+10 points bonus"}
 local function SonicScore10Callback(player_name, bonus)
 	tfm.exec.setPlayerScore(player_name, 10, true)
-	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y - 1, 0, -6, 0, 0.4, player_name)
-	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x - 1, bonus.y, 0, -6, 0, 0.4, player_name)
-	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y + 1, 0, -6, 0, 0.4, player_name)
-	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x + 1, bonus.y, 0, -6, 0, 0.4, player_name)
+	tfm.exec.displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y - 1, 0, -6, 0, 0.4, player_name)
+	tfm.exec.displayParticle(tfm.enum.particle.yellowGlitter, bonus.x - 1, bonus.y, 0, -6, 0, 0.4, player_name)
+	tfm.exec.displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y + 1, 0, -6, 0, 0.4, player_name)
+	tfm.exec.displayParticle(tfm.enum.particle.yellowGlitter, bonus.x + 1, bonus.y, 0, -6, 0, 0.4, player_name)
 end
 bonus_types["SonicScore10"] = {image = "185bbbc0145.png", func = SonicScore10Callback}
