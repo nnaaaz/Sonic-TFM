ae66d25f5cc3177901fe94ca62786b7efdfcc10d
Pshy0 <Pshy0@users.noreply.github.com>
2023-01-17 23:38:44 +0100

    added bonus particles

--- lua/bonus_score1.lua
+++ lua/bonus_score1.lua
@@ -9,5 +9,9 @@ local image_list = pshy.require("pshy.images.list")
 image_list["1848a17e166.png"] = {bonus = true, w = 27, h = 27, desc = "+1 points bonus"}
 local function SonicScore1Callback(player_name, bonus)
 	tfm.exec.setPlayerScore(player_name, 1, true)
+	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y - 1, 0, -6, 0, 0.4, player_name)
+	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x - 1, bonus.y, 0, -6, 0, 0.4, player_name)
+	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x, bonus.y + 1, 0, -6, 0, 0.4, player_name)
+	tfm_exec_displayParticle(tfm.enum.particle.yellowGlitter, bonus.x + 1, bonus.y, 0, -6, 0, 0.4, player_name)
 end
 bonus_types["SonicScore1"] = {image = "1848a17e166.png", func = SonicScore1Callback}
